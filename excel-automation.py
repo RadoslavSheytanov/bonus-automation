34c64f09b1e7ad391775f6b2324a607651f8c013
Radoslav Sheytanov <133638936+RadoslavSheytanov@users.noreply.github.com>
2023-07-18 09:47:00 +0100

    Update excel-automation.py

--- excel-automation.py
+++ excel-automation.py
@@ -58,10 +58,12 @@ if st.button('Process File'):
     elif bonus_type and bonus_code and name and platform:
         output_file_path = process_file(source_file, bonus_type, bonus_code, name, platform)
         if output_file_path:
+            with open(output_file_path, "rb") as f:
+                file_bytes = f.read()
             st.download_button(
                 label="Download Output File",
-                data=output_file_path,
-                file_name=os.path.basename(output_file_path)
+                data=file_bytes,
+                file_name=output_file_path.split('/')[-1]
             )
     else:
         st.error("All fields are required.")
