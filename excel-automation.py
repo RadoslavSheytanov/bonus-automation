d1de42275a57a481b2cb2e39e94282d8ec603449
Radoslav Sheytanov <133638936+RadoslavSheytanov@users.noreply.github.com>
2023-07-18 11:40:03 +0100

    Update excel-automation.py
    Added markdowns, streamlining the UI in Streamlit. Additional bug fixes and tweaks.

--- excel-automation.py
+++ excel-automation.py
@@ -42,7 +42,7 @@ def get_csv_download_link(csv_file):
     with open(csv_file, 'rb') as f:
         bytes = f.read()
         b64 = base64.b64encode(bytes).decode()
-        href = f'<a href="data:file/csv;base64,{b64}" download="{csv_file.split("/")[-1]}">Download CSV File</a>'
+        href = f'<a href="data:file/csv;base64,{b64}" download="{csv_file.split("/")[-1]}" style="display: inline-block; padding: 0.5em 1em; color: #ffffff; background-color: #007bff; border-radius: 0.25em; text-decoration: none;">Download CSV File</a>'
     return href
 
 st.title('Excel Automation')
@@ -59,13 +59,11 @@ if st.button('Process File'):
 
         # Display the non-VIP data
         if output_file_path is not None:
-            st.write(f"Non-VIP data has been saved to {output_file_path}")
             st.markdown(get_csv_download_link(output_file_path), unsafe_allow_html=True)
 
         # Display the VIP data
         if vip_data is not None and not vip_data.empty:
-            st.write("VIP Players (should be credited in a different campaign):")
-            for _, row in vip_data.iterrows():
-                st.write(f"{row['Sbuserid']} - {row['Sum of FS'].split('(')[1].split(')')[0]} FS")
+            st.markdown("**VIP Players (should be credited in a different campaign):**", unsafe_allow_html=True)
+            st.dataframe(vip_data)
     else:
         st.error("Please provide all inputs.")
