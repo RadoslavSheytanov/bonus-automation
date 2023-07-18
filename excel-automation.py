65ffc2b6a3caef30e5a456c87078d40c5190c2f8
Radoslav Sheytanov <133638936+RadoslavSheytanov@users.noreply.github.com>
2023-07-18 11:19:53 +0100

    Update excel-automation.py

--- excel-automation.py
+++ excel-automation.py
@@ -4,6 +4,7 @@ import tempfile
 import os
 import datetime
 import re
+import base64
 
 def process_file(source_file, bonus_type, bonus_code, name, platform):
     try:
@@ -28,13 +29,21 @@ def process_file(source_file, bonus_type, bonus_code, name, platform):
 
         # Save the non-VIP data to a temporary CSV file
         temp_dir = tempfile.mkdtemp()
-        output_file_path = os.path.join(temp_dir, f"{name}_{platform}_{bonus_type.replace(' ', '')}_{bonus_code.replace('ddmmy', datetime.datetime.now().strftime('%d%m%Y'))}.csv")
+        output_file_path = os.path.join(temp_dir, f"{name}_{platform}_{bonus_type.replace(' ', '')}_{bonus_code.replace('ddmmyy', datetime.datetime.now().strftime('%d%m%Y'))}.csv")
         non_vip_data.to_csv(output_file_path, index=False, header=bonus_type != 'Free Spins')
 
         return output_file_path, vip_data
     except Exception as e:
         st.error(f"An error occurred while processing the file: {str(e)}")
         return None, None
+
+def get_csv_download_link(csv_file):
+    # Generate a link to download the CSV file
+    with open(csv_file, 'rb') as f:
+        bytes = f.read()
+        b64 = base64.b64encode(bytes).decode()
+        href = f'<a href="data:file/csv;base64,{b64}" download="non_vip_data.csv">Download CSV File</a>'
+    return href
 
 st.title('Excel Automation')
 
@@ -51,10 +60,11 @@ if st.button('Process File'):
         # Display the non-VIP data
         if output_file_path is not None:
             st.write(f"Non-VIP data has been saved to {output_file_path}")
+            st.markdown(get_csv_download_link(output_file_path), unsafe_allow_html=True)
 
         # Display the VIP data
         if vip_data is not None and not vip_data.empty:
-            st.write("VIP Players:")
+            st.write("VIP Players (should be credited in a different campaign):")
             for _, row in vip_data.iterrows():
                 st.write(f"{row['Sbuserid']} - {row['Sum of FS'].split('(')[1].split(')')[0]} FS")
     else:
